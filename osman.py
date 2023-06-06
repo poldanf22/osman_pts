d4217b41d64d3b60d1e41843243e8b44a8c2c2f4
Poldanf <poldanf22@gmail.com>
2023-06-06 18:39:56 +0700

    osman temp

--- osman.py
+++ osman.py
@@ -61,8 +61,7 @@ with col9:
         "KURIKULUM",
         ("--Pilih Kurikulum--", "K13", "KM"))
 
-TAHUN = st.text_input("Masukkan Tahun Ajaran", placeholder="2022-2023",
-                      help="Contoh: 2022-2023")
+TAHUN = st.text_input("Masukkan Tahun Ajaran", placeholder="contoh: 2022-2023")
 
 col1, col2, col3, col4, col5 = st.columns(5)
 
