d0a6544806e055d8847b7bc5768ea396f86184a7
Poldanf <poldanf22@gmail.com>
2023-06-13 00:23:02 +0700

    kurmed oh kurmed

--- pages/osman_11km.py
+++ pages/osman_11km.py
@@ -55,7 +55,7 @@ with col9:
 TAHUN = st.text_input("Masukkan Tahun Ajaran", placeholder="contoh: 2022-2023")
 
 col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns(
-    7)
+    11)
 
 with col1:
     MTW = st.selectbox(
