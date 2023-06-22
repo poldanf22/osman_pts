c853fbd4b1752c08b4b15262e6bfa9e30c448f5f
Poldanf <poldanf22@gmail.com>
2023-06-22 14:03:17 +0700

    auth

--- osman.py
+++ osman.py
@@ -5,10 +5,10 @@ from pathlib import Path
 import subprocess
 
 # User Authentication
-names = ["TI Polda NF 1", "TI Polda NF 2"]
+names = ["TI NF", "Polda NF"]
 usernames = ["admin1", "admin2"]
 
-# Load hashed kd_akses
+# load hashed kd_akses
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_kd_akses = pickle.load(file)
@@ -17,33 +17,8 @@ authenticator = stauth.Authenticate(
     names, usernames, hashed_kd_akses, "lookup", "abcdef")
 name, authentication_status, username = authenticator.login("Login", "main")
 
-# Cek apakah pengguna sudah terotentikasi
-if "is_authenticated" not in st.session_state:
-    st.session_state.is_authenticated = False
+if authentication_status == False:
+    st.error("Username/kode akses salah!")
 
-# Menampilkan halaman yang sesuai berdasarkan status autentikasi
-if authentication_status:
-    st.session_state.is_authenticated = True
-
-if st.session_state.is_authenticated:
-    # Tambahkan file lain yang ingin diakses
-    selected_file = st.selectbox(
-        "Pilih file:", ("pivot.py", "nilai_std_sd_smp_10km.py"))
-
-    if st.button("Buka File"):
-        # Ganti folder_path dengan jalur folder yang berisi file-file tersebut
-        file_path = f"halaman/{selected_file}"
-        st.experimental_rerun()
-        st.stop()
-
-    # if st.button("Buka File"):
-    #     # Ganti folder_path dengan jalur folder yang berisi file-file tersebut
-    #     file_path = f"halaman/{selected_file}"
-    #     subprocess.Popen(["streamlit", "run", file_path])
-    #     st.warning("Mohon ditunggu sampai muncul Tab Baru!")
-else:
-    if authentication_status is False:
-        st.error("Username/Password is incorrect")
-
-    if authentication_status is None:
-        st.warning("Please enter your username and password")
+if authentication_status == None:
+    st.warning("Silahkan masukan username dan kode akses")
