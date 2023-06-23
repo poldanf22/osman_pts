60fa32aea07cfc2127e67c7b7af88642a65ea18a
Poldanf <poldanf22@gmail.com>
2023-06-23 17:46:44 +0700

    link

--- osman.py
+++ osman.py
@@ -34,10 +34,7 @@ if authentication_status:
     authenticator.logout("Logout", "sidebar")
     with st.sidebar:
         with st.sidebar:
-        st.markdown(f'''
-<a href={url}><button style="background-color:GreenYellow;">Untuk Lok.</button></a>
-''',
-                    unsafe_allow_html=True)
+        st.markdown(f'''<a href={url}><button style="background-color:GreenYellow;">Untuk Lok.</button></a>''',unsafe_allow_html=True)
         selected_file = option_menu(
             menu_title="Pilih file:",
             options=["Pivot", "Nilai Std. SD, SMP, 10KM", "Nilai Std. All IPA", "Nilai Std. 10, 11 IPS", "Nilai Std. PPLS, RONIN IPS", "Nilai Std. 11KM",
